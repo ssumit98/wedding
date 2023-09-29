8855b22e146ad17c591001b8251ea9dbe3c9e494
Sumit Sabbinwar <sumit.sabbinwar@gmail.com>
2023-09-29 15:21:01 +0530

    Add files via upload

---
+++ js/custome.js
@@ -0,0 +1,166 @@
+﻿( function($) {
+  'use strict';
+  	
+
+  	/* Window Load */
+	$(window).on('load',function(){
+		$('.loader').fadeOut(1000);
+        $('.line').addClass('active');
+	});
+
+
+    /* Navbar scroll*/
+    $('.navbar-nav ul li a').on('click', function() {
+        var target = $(this.hash);
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: (target.offset().top)
+            }, 1000);
+            $('body').removeClass('menu-is-opened').addClass('menu-is-closed');
+            return false;
+        }
+    });
+
+
+
+    
+
+    /* Full page scroll*/
+    if ($('#pagepiling').length > 0){
+
+        $('#pagepiling').pagepiling({
+            scrollingSpeed: 280,
+            navigation:false,
+            menu: '.navbar-nav',
+            anchors: ['home', 'counter', 'bride', 'groom','gallary' ,'video', 'events', 'contact', 'rsvp'],
+            afterRender: function(anchorLink, index){ 
+              NavbarColor();
+
+            },
+            afterLoad: function(anchorLink, index){
+                $('.pp-section .intro').removeClass('animate');
+                $('.active .intro').addClass('animate');
+                NavbarColor();
+            }
+        });
+
+  
+
+        function NavbarColor(){
+         if ($('.pp-section.active').hasClass('navbar-is-white')){
+                $('.navbar-desctop').addClass('navbar-white');
+                $('.progress-nav').addClass('progress-nav-white');
+                $('.navbar-bottom').addClass('navbar-bottom-white');
+            }
+            else{
+                $('.navbar-desctop').removeClass('navbar-white');
+                $('.progress-nav').removeClass('progress-nav-white');
+                $('.navbar-bottom').removeClass('navbar-bottom-white');
+            }
+        }
+    }
+
+    $('.popup-youtube').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+
+
+
+    /* Change bacgkround on project section*/
+    $('.project-box').on('mouseover',function(){
+        var index = $('.project-box').index(this);
+        $('.bg-changer .section-bg').removeClass('active').eq(index).addClass('active');
+    });
+
+    /* Carousel Gallary */
+    $('.carousel-gallary').owlCarousel({
+	    loop:true,
+	    margin:10,
+        nav:false,
+	    dots:true,
+	    items:3,
+        autoplay:true,
+        responsive : {
+            0 : {
+                items:1,
+            },
+            480 : {
+                items:2,
+            },
+            920 :{
+                items:3,
+            }
+        }
+	});
+    /* Carousel Event */
+    $('.carousel-testimonials').owlCarousel({
+	    loop:true,
+	    margin:10,
+        nav:true,
+	    dots:true,
+	    items:2,
+        responsive : {
+            0 : {
+                items:1,
+            },
+            480 : {
+                items:1,
+            },
+            768 : {
+                items:2,
+            },
+        }
+	});
+
+    /* Send form */
+	if ($('.js-ajax-form').length) {
+		$('.js-ajax-form').each(function(){
+			$(this).validate({
+				errorClass: 'error',
+			    submitHandler: function(form){
+		        	$.ajax({
+			            type: "POST",
+			            url:"mail.php",
+			            data: $(form).serialize(),
+			            success: function() {
+		                	$('#success-message').show();
+		                },
+
+		                error: function(){
+		                	$('#error-message').show();
+			            }
+			        });
+			    }
+			});
+		});
+	}
+    
+})(jQuery);
+function updateTimer() {
+    future  = Date.parse("Dec 31, 2023 18:00:00");
+    now     = new Date();
+    diff    = future - now;
+  
+    days  = Math.floor( diff / (1000*60*60*24) );
+    hours = Math.floor( diff / (1000*60*60) );
+    mins  = Math.floor( diff / (1000*60) );
+    secs  = Math.floor( diff / 1000 );
+  
+    d = days;
+    h = hours - days  * 24;
+    m = mins  - hours * 60;
+    s = secs  - mins  * 60;
+  
+    document.getElementById("clock")
+      .innerHTML =
+        '<div>' + d + '<span>days</span></div>' +
+        '<div>' + h + '<span>hours</span></div>' +
+        '<div>' + m + '<span>min</span></div>' +
+        '<div>' + s + '<span>sec</span></div>' ;
+  }
+setInterval('updateTimer()', 1000 );
